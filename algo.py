e05b826e945c3c47bc1c2000b9089a87cd6babb7
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 11:48:10 -0600

    iterate through the given string

--- algo.py
+++ algo.py
@@ -2,3 +2,10 @@
 #   Code Wars: DISEMVOWEL TROLLS
 ############################################
 
+def disemvowel(string_):
+
+    for char in string_:
+        print(char)
+    return string_
+
+print(disemvowel("Hello World!"))
