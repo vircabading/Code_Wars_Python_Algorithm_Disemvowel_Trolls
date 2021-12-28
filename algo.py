d0ec4245427ad5d47052116ca2d1aa40544f940c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 12:08:16 -0600

    make vowel check case insensitive

--- algo.py
+++ algo.py
@@ -5,8 +5,11 @@
 def disemvowel(string_):
     output = ""
     for char in string_:
-        if not (char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u') :
+        if not (char.lower() == 'a' or char.lower() == 'e' or 
+                char.lower() == 'i' or char.lower() == 'o' or 
+                char.lower() == 'u') :
             output += char
     return output
 
 print("DisEmVowel Hello World!:",disemvowel("Hello World!"))
+print("This website is for losers LOL!:",disemvowel("This website is for losers LOL!"))
