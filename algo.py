f4f3fd8573d84cd5ad8a8044b1efce4a92e93086
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-28 11:53:14 -0600

    find vowels via if statement

--- algo.py
+++ algo.py
@@ -3,9 +3,11 @@
 ############################################
 
 def disemvowel(string_):
-
+    output = ""
     for char in string_:
         print(char)
+        if (char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u') :
+            print ("Vowel found")
     return string_
 
 print(disemvowel("Hello World!"))
